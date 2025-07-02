2f09970e64adfac96b2995c579d7c3b0fe5cc346
João Victor <joaovictorsalotialves12@gmail.com>
2025-07-02 12:36:59 -0300

    Mapeando relacionamentos; Dependências externas

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import type { AnswerRepository } from '../repositories/answers-repository'
+import type { Answer } from '../entities/answer'
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswerRepository: AnswerRepository = {
+  create: async (answer: Answer) => {
+    return
+  },
+}
 
-  const answer = answerQuestion.execute({
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'Nova responsta',
