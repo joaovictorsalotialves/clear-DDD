bec5ce05846d8fefe5d3ba09c85ffbe56de66bf9
João Victor <joaovictorsalotialves12@gmail.com>
2025-07-03 14:33:31 -0300

    Classe base de entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '../../core/entities/entities'
 
 interface QuestionProps {
   title: string
@@ -7,18 +7,4 @@ interface QuestionProps {
   authorId: string
 }
 
-export class Question {
-  public id: string
-  public title: string
-  public slug: string
-  public content: string
-  public authorId: string
-
-  constructor(props: QuestionProps, id?: string) {
-    this.title = props.title
-    this.slug = props.slug
-    this.content = props.content
-    this.authorId = props.authorId
-    this.id = id ?? randomUUID()
-  }
-}
+export class Question extends Entity<QuestionProps> {}
