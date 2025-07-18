68f5c6b4f220c67ebc878ac11203f5314674320e
João Victor <joaovictorsalotialves12@gmail.com>
2025-07-18 10:54:59 -0300

    Functional Error Handling

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { type Either, left, right } from '@/core/either'
 import type { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
 
 interface DeleteCommentAnswerUseCaseRequest {
@@ -5,24 +6,28 @@ interface DeleteCommentAnswerUseCaseRequest {
   answerCommentId: string
 }
 
+type DeleteCommentAnswerUseCaseResponse = Either<string, {}>
+
 export class DeleteCommentAnswerUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
   async execute({
     authorId,
     answerCommentId,
-  }: DeleteCommentAnswerUseCaseRequest): Promise<void> {
+  }: DeleteCommentAnswerUseCaseRequest): Promise<DeleteCommentAnswerUseCaseResponse> {
     const answerComment =
       await this.answerCommentsRepository.findById(answerCommentId)
 
     if (!answerComment) {
-      throw new Error('Answer Comment not found.')
+      return left('Answer Comment not found.')
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      throw new Error('Not allowed.')
+      return left('Not allowed')
     }
 
     await this.answerCommentsRepository.delete(answerComment)
+
+    return right({})
   }
 }
